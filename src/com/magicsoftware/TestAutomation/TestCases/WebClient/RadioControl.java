10fb054e06793cb8c28f086c92fddea555138b62
Pranoday Dingare <pranodayd@magicsoftware.co.in>
2018-08-13 14:01:49 +0530

    Deleted java code

---
+++ src/com/magicsoftware/TestAutomation/TestCases/WebClient/RadioControl.java
@@ -0,0 +1,23 @@
+package com.magicsoftware.TestAutomation.TestCases.WebClient;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.magicsoftware.WebAutomation.WebTest;
+import com.magicsoftware.WebAutomation.Utils.Utilities;
+import com.magicsoftware.WebAutomation.Utils.Utilities.Appearancetype;
+import com.magicsoftware.WebAutomation.Utils.Utilities.SelectBy;
+
+public class RadioControl
+{
+	
+
+}	
+
